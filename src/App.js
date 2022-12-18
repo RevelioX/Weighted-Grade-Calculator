bad6b646492cef109e0712f6b8bb3a4fcbfad32d
Mariano.B <brardamariano12@gmail.com>
2022-12-17 21:18:09 -0300

    routing-test

--- src/App.js
+++ src/App.js
@@ -2,12 +2,21 @@
 import './App.css';
 import Theader from "./components/Theader.js"
 import Main from "./components/Main.js"
+import {Route, Routes, Link} from 'react-router-dom';
+
+function HelloWorld(){
+  return (
+  <div>
+    <p>Hola Mundo!</p>
+  </div>)
+}
 
 function App() {
   return (
     <div className="App">
-      <Theader/>
-      <Main></Main>
+      <Theader></Theader>
+      <Main/>
+      <Link to='/'>Enlace Hacia</Link>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,10 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App'
+import {createBrowserRouter, RouterProvider} from 'react-router-dom';
+
+function Holamundo(){
+  return(<h1>Hola Mundo</h1>)
+}
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello World</div>
+  },
+  {
+    path: "/banana",
+    element: <App/>
+  }
+]);
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
